8194966a6e3cb833df71638b478cba1656cf338f
xrowrobert <robert@xrow.de>
2014-05-12 11:25:58 +0200

    - forgotten operator added

--- autoloads/eztemplateautoload.php
+++ autoloads/eztemplateautoload.php
@@ -4,7 +4,7 @@ $eZTemplateOperatorArray = array();
 $eZTemplateOperatorArray[] = array( 
     'script' => 'extension/xrowmultiad/autoloads/xrowmultiadoperator.php' , 
     'class' => 'xrowMultiAdOperator' , 
-    'operator_names' => array( 'omsad', 'openxad', 'join_ad_basics' ) 
+    'operator_names' => array( 'omsad', 'openxad', 'join_ad_basics', 'ivw_addon' ) 
 );
 
 ?>
