f56af11b6336344a5df76747d8742e22952db57f
xrow <bjoern@xrow.de>
2014-08-13 11:05:15 +0200

    - CS

--- modules/oms/test.php
+++ modules/oms/test.php
@@ -1,14 +1,10 @@
 <?php
 
 $Module = $Params['Module'];
-#$http = eZHTTPTool::instance();
-$tpl = eZTemplate::factory();
-#$db = eZDB::instance();
 
-#$tpl->setVariable( 'var_name', eZUserAddition::recallUserID() );
+$tpl = eZTemplate::factory();
 
 $Result = array();
 $Result['content'] = $tpl->fetch( 'design:test.tpl' );
 $Result['path'] = array( array( 'url' => false,
                                 'text' => 'OMS Test-Modul' ) );
-?>
